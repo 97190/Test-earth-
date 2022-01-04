c39940e19cb66a3a18cbc4660bb49e62e7fc50e6
erika mayoute <e.mayoute44@gmail.com>
2022-01-04 14:22:55 +0100

    exo 03 is ok

---
+++ 03.rb
@@ -0,0 +1,9 @@
+# affichage de l'alphabet
+# a partir d'une lettre données en arguments
+# avec un retour à la ligne
+# utiliser une boucle while avec une condition de départ
+
+for n in ("n".."z")
+print "#{n}"
+end
+print "\n"
