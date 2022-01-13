1e0c6e51413a70c836953482fe1bcc8d5903df05
erika mayoute <e.mayoute44@gmail.com>
2022-01-13 16:17:19 +0100

    deuxième version

--- 01.rb
+++ 01.rb
@@ -5,6 +5,11 @@
 # __FILE__ == home/erika/.rbenv/shims/ruby
 
 # puts __FILE__
-File.basename("01.rb")
+# File.basename("01.rb")
+
+# puts __FILE__
+
+
+File.basename("/home/erika/epreuve de la terre/ruby.rb")
 
 puts __FILE__
