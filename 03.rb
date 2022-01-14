9dfd2f0cbf41af02757811ff7e5326cec05e98f6
erika mayoute <e.mayoute44@gmail.com>
2022-01-14 16:50:01 +0100

    push for harry

--- 03.rb
+++ 03.rb
@@ -1,10 +1,23 @@
 # affichage de l'alphabet
-# a partir d'une lettre données en arguments
 # avec un retour à la ligne
-# utiliser une boucle while avec une condition de départ
+# utiliser une boucle while avec une condition de dépar
 
-for n in ("n".."z")
-print "#{n}"
+
+ lettre_demandee = ARGV[0]
+
+#affichage de l'alphabet
+
+# tableau = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+#     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+
+#     puts tableau
+str = 'abcdefghijklmnopqrstuvwxyz'
+str.each_char { |c| print "#{c} " }
+
+if str && lettre_demandee
+    puts ARGV
+        else
+          puts "error"
 end
-print "\n"
-# L’alphabet à partir de
