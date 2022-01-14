97f4dfc263d609f4fd20c462c55d01ea306d6f5c
erika mayoute <e.mayoute44@gmail.com>
2022-01-14 09:38:42 +0100

    exo 01 nom du programme

--- 01.rb
+++ 01.rb
@@ -1,15 +1,2 @@
 # Nom du programme
-# if __FILE__ == $0
-
-#
-# __FILE__ == home/erika/.rbenv/shims/ruby
-
-# puts __FILE__
-# File.basename("01.rb")
-
-# puts __FILE__
-
-
-File.basename("/home/erika/epreuve de la terre/ruby.rb")
-
-puts __FILE__
+puts $0
