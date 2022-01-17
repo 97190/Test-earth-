236e24a567113958494ad5d115d9a87155885d39
erika mayoute <e.mayoute44@gmail.com>
2022-01-17 15:18:00 +0100

    Racine carrée 09

--- 09.rb
+++ 09.rb
@@ -1,6 +1,14 @@
 # Racine carrée d’un nombre#
-negativenumber = ARGV
-if not (ARGV[1])
+if ARGV[0].class == NilClass
+  puts "erreur"
+  exit
+end
+
+if ARGV[0].to_i < 0
+  puts "erreur"
+  exit 
+end
+
 racine = ARGV[0].to_i
 
 puts Math.sqrt(racine).to_i
