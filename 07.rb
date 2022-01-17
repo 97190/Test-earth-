21871ae1d27467698868a44f3d1029637d09ee46
erika mayoute <e.mayoute44@gmail.com>
2022-01-17 15:07:47 +0100

    suite

--- 07.rb
+++ 07.rb
@@ -1,12 +1,17 @@
 # Taille d'une chaine
 if ARGV[0].class == NilClass
-    puts "erreur"
-    exit
+  puts "erreur"
+  exit
 end
 
 if ARGV[0].to_i.to_s == ARGV[0]
-    puts "erreur"
-    exit
+  puts "erreur"
+  exit
 end
 
- puts ARGV[0].length
+if ARGV.count > 1
+  puts "erreur"
+  exit
+end
+
+puts ARGV[0].length
--- 09.rb
+++ 09.rb
@@ -1,6 +1,6 @@
-# Racine carrée d’un nombre
-# compter les chiffres du nombre a extraire et diviser par deux
-
+# Racine carrée d’un nombre#
+negativenumber = ARGV
+if not (ARGV[1])
 racine = ARGV[0].to_i
 
-puts Math.sqrt(racine)
+puts Math.sqrt(racine).to_i
